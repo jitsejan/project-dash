dda4fef819a117530450466c996be986623d0008
Jitse-Jan <code@jitsejan.com>
2020-06-14 03:37:12 +0100

    Updated Github retriever

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ import os
 
 import confuse
 
+from githubretriever import GitHubRetriever
 from jiraretriever import JiraRetriever
 
 APPNAME = "PROJECTDASH"
@@ -12,6 +13,8 @@ APPNAME = "PROJECTDASH"
 
 def get_cli():
     argp = argparse.ArgumentParser()
+    argp.add_argument("-j", "--jira_retrieve", action="store_true", default=False)
+    argp.add_argument("-g", "--github_retrieve", action="store_true", default=False)
     argp.add_argument("-o", "--output", action="store_true", default=False)
     args = argp.parse_args()
     return args
@@ -24,13 +27,25 @@ def get_config():
 
 
 def main(config):
-    retr = JiraRetriever(
-        board_id=config["jira"]["board_id"].get(int),
-        project=config["jira"]["project"].get(),
-    )
-    if config["output"]:
-        print(retr.get_sprints_for_board_id())
-        print(retr.get_issues_for_project())
+    if config["jira_retrieve"].get():
+        retr = JiraRetriever(
+            board_id=config["jira"]["board_id"].get(int),
+            project=config["jira"]["project"].get(),
+        )
+        sprints = retr.get_sprints_dataframe()
+        issues = retr.get_issues_dataframe()
+        if config["output"]:
+            print(issues)
+            print(sprints)
+    if config["github_retrieve"].get():
+        retr = GitHubRetriever(
+            repos=config["github"]["repos"].as_str_seq(),
+        )
+        branches = retr.get_branches_dataframe()
+        pulls = retr.get_pulls_dataframe()
+        if config["output"]:
+            print(branches)
+            print(pulls)
 
 
 if __name__ == "__main__":
