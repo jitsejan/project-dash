a9188a9df09cdfce5317b57e39df92007bc24827
Jitse-Jan <code@jitsejan.com>
2020-06-16 13:58:25 +0100

    Ran black and isort

--- index.py
+++ index.py
@@ -1,21 +1,20 @@
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
-
 from app import app
-from apps import jira, github
-
+from apps import github, jira
 
 PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
-nav_items = dbc.Container([
-    dbc.NavItem(dbc.NavLink("Home", href='/', id="page-1-link")),
-    dbc.NavItem(dbc.NavLink("Jira", href='/jira', id="page-2-link")),
-    dbc.NavItem(dbc.NavLink("Github", href='/github', id="page-3-link"))
-])
+nav_items = dbc.Container(
+    [
+        dbc.NavItem(dbc.NavLink("Home", href="/", id="page-1-link")),
+        dbc.NavItem(dbc.NavLink("Jira", href="/jira", id="page-2-link")),
+        dbc.NavItem(dbc.NavLink("Github", href="/github", id="page-3-link")),
+    ]
+)
 dropdown = dbc.DropdownMenu(
     children=[
         dbc.DropdownMenuItem(dbc.NavLink("Jira Board", href="#")),
@@ -43,9 +42,7 @@ navbar = dbc.Navbar(
             ),
             dbc.NavbarToggler(id="navbar-toggler"),
             dbc.Collapse(
-                dbc.Nav(
-                    [nav_items, dropdown], className="ml-auto", navbar=True
-                ),
+                dbc.Nav([nav_items, dropdown], className="ml-auto", navbar=True),
                 id="navbar-collapse",
                 navbar=True,
             ),
@@ -56,73 +53,62 @@ navbar = dbc.Navbar(
     className="mb-5",
 )
 
-layout = html.Div([
-    html.Div([
-        html.H1(children='Project overview', style={'textAlign': 'center',}),
-        dbc.Row([
-            dbc.Col(html.Img(src="assets/github.PNG", className="row-img")),
-        ]),
-        dbc.Row(
+layout = html.Div(
+    [
+        html.Div(
             [
-                dbc.Col(github.open_pulls_table),
-            ],
-            align="center",
-        ),
-        dbc.Row([
-            dbc.Col(html.Img(src="assets/jira.PNG", className="row-img")),
-        ]),
-        dbc.Row(
-            dbc.Col(html.H3(children='In progress', style={'textAlign': 'center',}),),
-        ),
-        dbc.Row(
-            [
-                dbc.Col(jira.open_issues_table),
-            ],
-            align="center",
-        ),
-        dbc.Row(
-            dbc.Col(html.H3(children='In review', style={'textAlign': 'center',}),),
-        ),
-        dbc.Row(
-            [
-                dbc.Col(jira.review_issues_table),
-            ],
-            align="center",
-        ),
-        dbc.Row(
-            [
-                dbc.Col(html.H3(children='Todo', style={
-                    'textAlign': 'center',
-                }),),
-            ],
-            align="center",
-        ),
-        dbc.Row(
-            [
-                dbc.Col(jira.todo_issues_table),
-            ],
-            align="center",
-        ),
-    ])
-]) 
+                html.H1(children="Project overview", style={"textAlign": "center",}),
+                dbc.Row(
+                    [dbc.Col(html.Img(src="assets/github.PNG", className="row-img")),]
+                ),
+                dbc.Row([dbc.Col(github.open_pulls_table),], align="center",),
+                dbc.Row(
+                    [dbc.Col(html.Img(src="assets/jira.PNG", className="row-img")),]
+                ),
+                dbc.Row(
+                    dbc.Col(
+                        html.H3(children="In progress", style={"textAlign": "center",}),
+                    ),
+                ),
+                dbc.Row([dbc.Col(jira.open_issues_table),], align="center",),
+                dbc.Row(
+                    dbc.Col(
+                        html.H3(children="In review", style={"textAlign": "center",}),
+                    ),
+                ),
+                dbc.Row([dbc.Col(jira.review_issues_table),], align="center",),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            html.H3(children="Todo", style={"textAlign": "center",}),
+                        ),
+                    ],
+                    align="center",
+                ),
+                dbc.Row([dbc.Col(jira.todo_issues_table),], align="center",),
+            ]
+        )
+    ]
+)
 
-app.layout = dbc.Container([
-    navbar,
-    dbc.Container([
-         dcc.Location(id='url', refresh=False),
-         html.Div(id='page-content')
-    ])
-], fluid=True)
+app.layout = dbc.Container(
+    [
+        navbar,
+        dbc.Container(
+            [dcc.Location(id="url", refresh=False), html.Div(id="page-content")]
+        ),
+    ],
+    fluid=True,
+)
 
 
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def display_page(pathname):
-    if pathname == '/':
+    if pathname == "/":
         return layout
-    elif pathname == '/jira':
+    elif pathname == "/jira":
         return jira.layout
-    elif pathname == '/github':
+    elif pathname == "/github":
         return github.layout
     return dbc.Jumbotron(
         [
@@ -132,17 +118,20 @@ def display_page(pathname):
         ]
     )
 
+
 def toggle_navbar_collapse(n, is_open):
     """ Callback to toggle the collapse on small screens """
     if n:
         return not is_open
     return is_open
 
+
 app.callback(
     Output(f"navbar-collapse", "is_open"),
     [Input(f"navbar-toggler", "n_clicks")],
     [State(f"navbar-collapse", "is_open")],
 )(toggle_navbar_collapse)
+
 
 @app.callback(
     [Output(f"page-{i}-link", "active") for i in range(1, 4)],
@@ -151,11 +140,12 @@ app.callback(
 def toggle_active_links(pathname):
     if pathname == "/":
         return True, False, False
-    elif pathname == "/apps/jira":
+    elif pathname == "/jira":
         return [False, True, False]
-    elif pathname == "/apps/github":
+    elif pathname == "/github":
         return [False, False, True]
     return False, False, False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run_server(debug=True)
